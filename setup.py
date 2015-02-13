26c177e2aef4a4802958e195d00789bd67415562
Fernando Freitas Alves <ffreitasalves@gmail.com>
2015-02-13 15:32:18 -0200

    colocando o auto-update

--- setup.py
+++ setup.py
@@ -1,13 +1,30 @@
+import esky.bdist_esky
+from esky.bdist_esky import Executable as Executable_Esky
 from cx_Freeze import setup, Executable
 
+include_files = ['boneca.jpg','chuck.jpg']
+
 setup(
-    name = "boneca",
-    version = "1.0.0",
-    options = {"build_exe": {
-        'packages': ["os","sys","ctypes","win32con"],
-        'excludes': ['tkinter','tcl','ttk'],
-        'include_files': ['boneca.jpg'],
-        'include_msvcr': True,
-    }},
-    executables = [Executable("boneca.py",base="Win32GUI")]
+    name = 'boneca',
+    version = '1.0.2',
+    options = {
+        'build_exe': {
+            'packages': ['os','sys','ctypes','win32con'],
+            'excludes': ['tkinter','tcl','ttk'],
+            'include_files': include_files,
+            'include_msvcr': True,
+        },
+        'bdist_esky': {
+            'freezer_module': 'cx_freeze',
+        }
+    },
+    data_files = include_files,
+    scripts = [
+        Executable_Esky(
+            "boneca.py",
+            gui_only = True,
+            #icon = XPTO #Coloque um icone aqui se quiser ,
+            ),
+    ],
+    executables = [Executable('boneca.py',base='Win32GUI')]
     )
